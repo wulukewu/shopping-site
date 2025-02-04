b94fc81fa863ab48b98c73b9ff6de019e54635b4
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-04 23:11:21 +0800

    feat: add products list

---
+++ src/components/products/ProductsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="products">
+    <ul>
+        <li v-for="product in products" :key="product.id">
+            <!-- <img :src="product.image" :alt="product.name" /> -->
+            <h3>{{ product.name }}</h3>
+            <p>{{ product.description }}</p>
+            <p>{{ product.price }}</p>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+    props: {
+        products: {
+            type: Array,
+            required: true,
+        },
+    },
+}
+</script>
