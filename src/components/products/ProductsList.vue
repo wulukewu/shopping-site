dff7f137c8dcbf01fe6df8161469faf410c92f7e
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-04 23:21:44 +0800

    feat: fix products' id

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="products">
     <ul>
-        <li v-for="product in products" :key="product.id">
-            <!-- <img :src="product.image" :alt="product.name" /> -->
-            <h3>{{ product.name }}</h3>
-            <p>{{ product.description }}</p>
-            <p>{{ product.price }}</p>
-        </li>
+      <li v-for="product in products" :key="product.id">
+        <!-- <img :src="product.image" :alt="product.name" /> -->
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default{
-    props: {
-        products: {
-            type: Array,
-            required: true,
-        },
+export default {
+  props: {
+    products: {
+      type: Array,
+      required: true,
     },
-}
+  },
+};
 </script>
