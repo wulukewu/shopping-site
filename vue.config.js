bb96b3958577281ed181b55c14bed1ab061de2ca
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-05 15:08:46 +0800

    feat: fix Single Page Applications (SPAs) deployed to GitHub Pages by 404.html Redirect

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/shopping-site/'
-    : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/shopping-site/" : "/",
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "public", to: "." }, // Copies the entire 'public' directory to the build output.
+        ],
+      }),
+    ],
+  },
 };
