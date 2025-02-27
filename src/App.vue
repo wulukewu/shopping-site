cb38052820f28ec88f1da5c72771110a0fa4881b
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-27 08:38:48 +0800

    fix: console log apiUrl

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,8 @@ export default {
       try {
         const token = localStorage.getItem('token');
         const apiUrl = process.env.VUE_APP_API_URL;
-
+        
+        console.log('api URL:', apiUrl);
         const response = await fetch(`${apiUrl}/products`, {
           method: 'GET',
           headers: {
