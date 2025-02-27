e487766e9a2d5ef0c1a2734c668133111b1c828e
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-27 09:05:12 +0800

    fix: back to publicPath with /

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  publicPath: './',
+  publicPath: '/',
 
   configureWebpack: {
     plugins: [
